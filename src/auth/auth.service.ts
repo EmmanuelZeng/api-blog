1bb857d4c4d220961f95d80d26a009d7dce33958
EmmanuelZeng <emmazeng38@gmail.com>
2025-06-23 15:33:41 +0200

    feat:add login function

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,36 @@
 import { authUserDto } from './dto/auth-user.dto';
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
+import { compare } from 'bcrypt';
+import { error } from 'console';
 import { User } from 'src/users/entities/user.entity';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
     constructor(
-        private readonly userService: UsersService
+        private readonly userService: UsersService,
+        private readonly jwtService: JwtService
     ){}
 
     async login(authBody: authUserDto) {
         const { email, password } = authBody;
-        const existingUser = await this.userService.getUserByEmail(email)
+        const existingUser = await this.userService.getUserByEmail(email);
+
+        if(!existingUser) throw new UnauthorizedException({error: "Mot de passe ou nom  d'utilisateur incorrect"})
+        
+        const isPasswordValid = await this.isPasswordValid(password, existingUser.password);
+        if(!isPasswordValid) throw new UnauthorizedException({error: "Mot de passe ou nom  d'utilisateur incorrect"})
+        return this.authenticateUser({id : existingUser.id})
+    }
+
+    private async isPasswordValid(password:string, hashedPassword: string): Promise<boolean> {
+        return await compare(password, hashedPassword);
+    }
+
+    private async authenticateUser({id}: {id: number}){
+        const payload = { id };
+        return { access_token: await this.jwtService.sign(payload) }
     }
 }
