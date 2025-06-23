1291d9c92aaee7ea05b6b3ee190dba64d43fe948
EmmanuelZeng <emmazeng38@gmail.com>
2025-06-23 15:24:09 +0200

    feat:import User and Article entity

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { ArticlesController } from './articles.controller';
+import { UsersService } from 'src/users/users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
+import { Article } from './entities/article.entity';
 
 @Module({
-  providers: [ArticlesService],
+  imports: [TypeOrmModule.forFeature([User, Article])],
+  providers: [ArticlesService, UsersService],
   controllers: [ArticlesController]
 })
 export class ArticlesModule {}
