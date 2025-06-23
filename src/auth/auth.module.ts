280f2a6e5acd5f6352f406689482040cc5f379f8
EmmanuelZeng <emmazeng38@gmail.com>
2025-06-23 15:28:44 +0200

    feat:add JWT module for auth login

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,22 @@ import { AuthService } from './auth.service';
 import { UsersService } from 'src/users/users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/users/entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        global: true,
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '15d' }
+      })
+    })
+],
   controllers: [AuthController],
   providers: [AuthService, UsersService]
 })
