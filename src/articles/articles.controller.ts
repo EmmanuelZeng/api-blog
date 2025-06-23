d5549a7a41d05f80b45c0db56c3f19c06066832a
EmmanuelZeng <emmazeng38@gmail.com>
2025-06-23 15:22:33 +0200

    feat: add function createArticle and getAllArticles

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { createArticleDto } from './dto/create-article.dto';
+import { ArticlesService } from './articles.service';
 
 @Controller('articles')
-export class ArticlesController {}
+export class ArticlesController {
+    constructor(
+        private readonly articleService: ArticlesService
+    ){}
+    @Post('create')
+    createArticle(@Body() createArticleDto: createArticleDto) {
+        return this.articleService.createArticle(createArticleDto)
+    }
+
+    @Get()
+    getAllArticles(){
+        return this.articleService.getAllArticles();
+    }
+}
