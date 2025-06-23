eb67611296b5217e6a3b2bfed2227f8b9c1662a1
EmmanuelZeng <emmazeng38@gmail.com>
2025-06-23 15:51:09 +0200

    feat:add functions

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,6 +4,7 @@ import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { createUserDto } from './dto/create-user.dto';
 import { authUserDto } from 'src/auth/dto/auth-user.dto';
+import { hash } from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -12,19 +13,32 @@ export class UsersService {
         private readonly userRepository: Repository<User>
     ){}
 
-    async create(data: createUserDto){
-        const user = this.userRepository.create(data);
-        console.log(user);
-        await this.userRepository.save(user);
-        return `L'utilisateur ${user.name} a été créé`;
+    async create(user: createUserDto){
+        const userHashPassword = await this.hashPassword(user.password)
+        try {
+            await this.userRepository.save({...user, password: userHashPassword});
+            console.log(user);
+            return `L'utilisateur ${user.name} a été créé`;
+        } catch (error) {
+            throw new Error("Impossible de créer l'utilisateur")
+        }
+        
     }
 
+    private async hashPassword(password: string) {
+        const hashedPassword = await hash(password, 9);
+        return hashedPassword;
+    }
     async getUsers(){
         const users = await this.userRepository.find()
         return users;
     }
     async getUserByEmail(email: string){
-        const user = await this.userRepository.findOne({where : {email}})
+        const user = await this.userRepository.findOneBy({['email'] : email})
+        return user;
+    }
+    async findById(id: number) {
+        const user = await this.userRepository.findOneBy({['id'] : id})
         return user;
     }
 }
