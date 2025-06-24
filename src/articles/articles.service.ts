c6569ee7272fdda61c6d37266f8747b10553312c
EmmanuelZeng <emmazeng38@gmail.com>
2025-06-24 16:37:35 +0200

    feat:add findOne, remove and update methods

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,9 +1,10 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UsersService } from 'src/users/users.service';
 import { Article } from './entities/article.entity';
 import { Repository } from 'typeorm';
 import { createArticleDto } from './dto/create-article.dto';
+import { updateArticleDTO } from './dto/update-article.dto';
 
 @Injectable()
 export class ArticlesService {
@@ -17,10 +18,6 @@ export class ArticlesService {
     async createArticle(article: createArticleDto) {
         
         const user = await this.userService.findById(article.authorId);
-        if (!user) {
-            throw new Error(`Utilisateur avec l'id ${article.authorId} introuvable`);
-        }
-
         try {
             const articleEntity = this.articleRepository.create({
                 title: article.title,
@@ -50,6 +47,47 @@ export class ArticlesService {
             }
         });
         return articles;
+    }
+    async findOne(id: string) {
+        try {
+            console.log("Recherche de l'article avec l'id :", id);
+            // Vérifier si l'article avec l'id existe dans la base de données
+            const article = await this.articleRepository.createQueryBuilder('article')
+                .leftJoinAndSelect('article.author', 'author')
+                .where('article.id =:id', {id: id})
+                .getOne();
+
+            if(!article) return (`Article avec l'id ${id} non trouvé.`);
+            const { title, content, createdAt, author } = article;
+            
+            return article;
+        } catch (error) {
+            console.error("Erreur lors de la récupération de l'article :", error);
+            throw new InternalServerErrorException("Une erreur est subvenue lors de la récupération de l'article");
+        }
+    }
+
+    async getOne(id: string) {
+        const article = this.articleRepository.findOneOrFail({
+            where: { id },
+            relations: ['author']
+        });
+        if(!article) throw new HttpException(`Impossible de trouver l'article`, HttpStatus.NOT_FOUND);
+        return article;
+    }
+
+    async remove(id: string){
+        const existingArticle = await this.getOne(id);
+        if(!existingArticle) throw new HttpException(`Article non trouvé`, HttpStatus.BAD_REQUEST);
+        await this.articleRepository.remove(existingArticle);
+        return existingArticle;
+    }
+
+    async update(id: string, updateArticleDTO: updateArticleDTO) {
+        await this.getOne(id);
+        await this.articleRepository.update(id, updateArticleDTO);
+        console.log(`L'article a été mis à jour`);
+        return this.findOne(id);
     }
 
 }
