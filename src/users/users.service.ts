ded6cc94df5a3ec3e45e5b91e3a3937f08019195
EmmanuelZeng <emmazeng38@gmail.com>
2025-06-25 16:20:13 +0200

    feat: add function hashPassword

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,19 @@
+import { hashPassword } from './../utils/hashPassword';
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { createUserDto } from './dto/create-user.dto';
-import { hash } from 'bcrypt';
 
 @Injectable()
 export class UsersService {
     constructor(
         @InjectRepository(User)
-        private readonly userRepository: Repository<User>
+        private readonly userRepository: Repository<User>,
     ){}
 
     async create(user: createUserDto){
-        const userHashPassword = await this.hashPassword(user.password)
+        const userHashPassword = await hashPassword(user.password)
         try {
             await this.userRepository.save({...user, password: userHashPassword});
             console.log(user);
@@ -22,11 +22,6 @@ export class UsersService {
             throw new Error("Impossible de créer l'utilisateur")
         }
         
-    }
-
-    private async hashPassword(password: string) {
-        const hashedPassword = await hash(password, 9);
-        return hashedPassword;
     }
     async getUsers(){
         const users = await this.userRepository.find()
