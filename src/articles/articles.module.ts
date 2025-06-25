2d805d6cafa7800e840592d640bd484618a0f00e
EmmanuelZeng <emmazeng38@gmail.com>
2025-06-25 15:43:37 +0200

    feat:add user module

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -3,12 +3,16 @@ import { ArticlesService } from './articles.service';
 import { ArticlesController } from './articles.controller';
 import { UsersService } from 'src/users/users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from 'src/users/entities/user.entity';
 import { Article } from './entities/article.entity';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Article])],
-  providers: [ArticlesService, UsersService],
-  controllers: [ArticlesController]
+  imports: [
+    TypeOrmModule.forFeature([Article]),
+    UsersModule,
+  ],
+  providers: [ArticlesService],
+  controllers: [ArticlesController],
+  exports: [ArticlesService, TypeOrmModule]
 })
 export class ArticlesModule {}
