e53cca426a38794c8f3035cc00eaca08e6de5959
EmmanuelZeng <emmazeng38@gmail.com>
2025-06-26 13:31:25 +0200

    feat:add swagger documentation for properties

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,12 +1,16 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString } from "class-validator";
 
 export class createUserDto {
+    @ApiProperty({type: 'string', example: "John Doe", description: "Nom complet de l'utilisateur"})
     @IsString()
     name: string
 
+    @ApiProperty({type: 'string', example: "johndoe@gmail.com", description: "L'email de l'utilisateur"})
     @IsEmail()
     email: string
 
+    @ApiProperty({type: 'string', example: "1234567890", description: "Mot de passe de l'utilisateur"})
     @IsString()
     password: string
 }
