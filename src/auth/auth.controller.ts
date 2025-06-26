50c24302e6cc32e5e7836290a8f27c41cc48af8b
EmmanuelZeng <emmazeng38@gmail.com>
2025-06-26 13:24:26 +0200

    feat:add swagger documentation on the route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,19 +2,26 @@ import { Body, Controller, Get, Post, Request, UseGuards } from '@nestjs/common'
 import { AuthService } from './auth.service';
 import { authUserDto } from './dto/auth-user.dto';
 import { AuthGuard } from './guard/auth.guard';
+import { ApiBearerAuth, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+
+@ApiTags('Authentifications')
 @Controller('auth')
 export class AuthController {
     constructor(
         private readonly authService: AuthService
     ){}
 
+    @ApiOperation({summary: "utilisée pour connecter un utilisateur"})
+    @ApiResponse({status: 201, description: "connexion réussite", type: authUserDto})
     @Get('login')
     login(@Body() authUserDto: authUserDto) {
         return this.authService.login(authUserDto)
     }
 
+    @ApiOperation({summary: "utilisé pour récupérer le profile d'uitlisateur"})
     @UseGuards(AuthGuard)
+    @ApiBearerAuth()
     @Get('profle')
     getProfile(@Request() req) {
         return this.authService.getProfile(req)
