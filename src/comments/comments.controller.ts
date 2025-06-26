cac35d3d69847e4f22202534a220cfbca34d110d
EmmanuelZeng <emmazeng38@gmail.com>
2025-06-26 13:27:08 +0200

    feat:add swagger documentation for all routes comment

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from 'src/auth/guard/auth.guard';
+import { createArticleDto } from 'src/articles/dto/create-article.dto';
 
+@ApiTags("Commentaires")
 @Controller('comments')
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
+  @ApiOperation({summary: "création de l'article"})
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiResponse({status: 200, type: createArticleDto})
+  @ApiBody({type: createArticleDto})
   @Post('create')
   create(@Body() createCommentDto: CreateCommentDto) {
     return this.commentsService.create(createCommentDto);
@@ -17,16 +26,26 @@ export class CommentsController {
     return this.commentsService.findAll();
   }
 
+  @ApiOperation({summary: "récupère un commentaire en particulier"})
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.commentsService.findOne(+id);
   }
 
+  @ApiOperation({summary: "Mise à jour du commentaire"})
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiParam({name: "id", description: "l'identifiant du commentaire"})
+  @ApiBody({type: UpdateCommentDto})
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
     return this.commentsService.update(id, updateCommentDto);
   }
 
+  @ApiOperation({summary: "Suppression du commentaire"})
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiParam({name: "id", description: "l'identifiant du commentaire"})
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.commentsService.remove(id);
