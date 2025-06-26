72edd1d118d6de260471aad6dfcd9f4ccd8330e2
EmmanuelZeng <emmazeng38@gmail.com>
2025-06-26 13:32:42 +0200

    feat:add swagger documentation for all routes users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,23 @@
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { createUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
 import { Body, Controller, Get, Post } from '@nestjs/common';
 
 @Controller('users')
+@ApiTags('Utilisateurs')
 export class UsersController {
     constructor(private readonly usersService: UsersService){}
+
     @Post('create')
+    @ApiOperation({summary: "Créer un utilisateur"})
+    @ApiResponse({status: 200, description: "utilisateur créé avec succès"})
+    @ApiBody({type: createUserDto})
     create(@Body() data: createUserDto) {
         return this.usersService.create(data);
     }
 
+    @ApiOperation({summary: "Créer un utilisateur"})
+    @ApiBody({type: [createUserDto]})
     @Get()
     getUser() {
         return this.usersService.getUsers();
