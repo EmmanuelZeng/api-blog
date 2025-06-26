e50bef668fed5bf28995d78a97b731275473e2ca
EmmanuelZeng <emmazeng38@gmail.com>
2025-06-26 14:11:13 +0200

    fix:delete export TypeOrmModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -19,7 +19,7 @@ import { UsersModule } from '../users/users.module';
         secret: configService.get<string>('JWT_SECRET'),
         signOptions: { expiresIn: '15d' }
       })
-    })
+    }),
   ],
   controllers: [AuthController],
   providers: [AuthService],
