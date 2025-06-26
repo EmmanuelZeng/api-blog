e0cf7b36e6ca2a9032c70f349d10ba280a6e2dd8
EmmanuelZeng <emmazeng38@gmail.com>
2025-06-26 12:26:20 +0200

    feat:add swagger documentation for properties

--- src/articles/dto/create-article.dto.ts
+++ src/articles/dto/create-article.dto.ts
@@ -1,13 +1,18 @@
-import { IsNotEmpty, IsString } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
 
 export class createArticleDto {
+    @ApiProperty({type: 'string', example: "Comment utiliser l'api blog", description: "titre de l'article"})
     @IsString()
     @IsNotEmpty()
     title:string
 
+    @ApiProperty({example: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.", description: "Contenu de l'article"})
+    @IsOptional()
     @IsNotEmpty()
     content:string
 
+    @ApiProperty({description: "identifiant de l'auteur de l'article", example: "1"})
     @IsNotEmpty()
     authorId:string
 }
