8cf82fabe04e2f83c734f628542cf2f9c9d0ae1c
EmmanuelZeng <emmazeng38@gmail.com>
2025-06-27 17:59:34 +0200

    feat:add functions

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,44 +1,86 @@
-import { authUserDto } from './dto/auth-user.dto';
-import { HttpException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { compare, hash } from 'bcrypt';
 import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
     constructor(
-        private readonly userService: UsersService,
-        private readonly jwtService: JwtService
+        private readonly jwtServices: JwtService
     ){}
 
-    //Fonction login user
-    async login(authBody: authUserDto) {
-        const { email, password } = authBody;
-        const existingUser = await this.userService.getUserByEmail(email);
+    //Génération d'une clé de cryptage
+    async generateSalt() {
+        const salt = await bcrypt.genSalt()
+    }
 
-        if(!existingUser) throw new UnauthorizedException({error: "Mot de passe ou nom  d'utilisateur incorrect"})
+    //Cryptage de refrech token
+    async hashRefreshToken(refresh_token: string): Promise<string> {
+        const hash = await bcrypt.hash(refresh_token, 9);
+        return hash;
+    }
+
+    //Test de validate de mot de passe
+    async validatePassword(incomePassword: string, existPassword: string): Promise<boolean> {
+        const isMatch = await bcrypt.compare(incomePassword, existPassword);
+        if(!isMatch) {
+        throw new HttpException(`Le mot de passe que vous avez entré est incorrect !`, HttpStatus.NOT_ACCEPTABLE);
+        }
+        return isMatch;
+    }
+
+    //Création de token
+    async sign(payload: Record<string, any>): Promise<string> {
+        const token = this.jwtServices.sign(payload);
+        return token;
+    }
+
+    //Test de validation de refresh token
+    async validateRefreshToken(incomeToken: string, existToken: string): Promise<boolean> {
+        const isMatch = await bcrypt.compare(incomeToken, existToken);
+        if (!isMatch) {
+        throw new HttpException(`Accès réfusé !`, HttpStatus.FORBIDDEN);
+        }
+        return isMatch;
+    }
+
+    //Creation de token d'accès
+    async signAccessToken(payload: Record<string, any>): Promise<string> {
+
+        return  this.jwtServices.signAsync(payload,
+            {
+            secret: process.env.JWT_ACCESS_SECRET,
+            expiresIn: '7500m',
+            },
+        )
+    }
+
+
+    //Création de token de rafraichissement
+    async signRefreshToken(payload: Record<string, any>): Promise<string> {
+
+        return  this.jwtServices.signAsync(payload,
+            {
+            secret: process.env.JWT_REFRESH_SECRET,
+            expiresIn: '7d',
+            },
+        )
         
-        const isPasswordValid = await this.isPasswordValid(password, existingUser.password);
-        if(!isPasswordValid) throw new UnauthorizedException({error: "Mot de passe ou nom  d'utilisateur incorrect"})
-        return this.authenticateUser({id : existingUser.id})
     }
 
-    //fonction récupérer le profile de l'utilisateur
-    async getProfile(name: string) {
-        const user = await this.userService.findById(name);
-        if(!user) throw new HttpException('Utilisateur non trouvé', HttpStatus.FORBIDDEN);
-
-        return {username: user.name, userEmail : user.email}
+    //Verification de token d'accès
+    async verifyAccessToken(token: string){
+        const result = await this.jwtServices.verify(token, {
+        secret: process.env.JWT_ACCESS_SECRET
+        })
+        return result
     }
 
-    //Fonction qui vérifie le password inséré par le user et celle dans la base de données
-    private async isPasswordValid(password:string, hashedPassword: string): Promise<boolean> {
-        return await compare(password, hashedPassword);
-    }
-
-    //Fonction qui gère le JWT 
-    private async authenticateUser({id}: {id: string}){
-        const payload = { id };
-        return { access_token: await this.jwtService.sign(payload) }
+    //Verification de token d'accès
+    async verifyRefreshToken(token: string){
+        const result = await this.jwtServices.verify(token,{
+        secret: process.env.JWT_REFRESH_SECRET
+        })
+        return result
     }
 }
